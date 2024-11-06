060e25cbc25bd08b7414c3405011f826322c526e
Suelol <kamalovramil432@gmail.com>
2024-11-06 16:56:18 +0300

    add setting connection

---
+++ Study_Kamalov_wpf_320P/Connection/ConnectionHelper.cs
@@ -0,0 +1,14 @@
+﻿using Study_Kamalov_wpf_320P.DbModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study_Kamalov_wpf_320P.Connection
+{
+    public static class ConnectionHelper
+    {
+        public static Study_Baza db = new Study_Baza();
+    }
+}
